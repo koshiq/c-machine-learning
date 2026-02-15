01331dca7f8aee7a07541e58748783916383fbcb
koshiq <koshiqhossain@gmail.com>
2026-02-14 22:24:35 -0500

    initial

---
+++ prng.h
@@ -0,0 +1,13 @@
+typedef struct {
+    u64 state;
+    u64 inc;
+} prng_state;
+
+void prng_seed_r(prng_state* rng, u64 initstate, u64 initseq);
+void prng_seed(u64 initstate, u64 initseq);
+
+u32 prng_rand_r(prng_state* rng);
+u32 prng_rand(void);
+
+f32 prng_randf_r(prng_state* rng);
+f32 prng_randf(void);
